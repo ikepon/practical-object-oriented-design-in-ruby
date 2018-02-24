60022e8bd5352b00f9dab34c89a160cee00e08b9
ikepon <tatsuyanoheya@gmail.com>
2018-02-24 15:09:56 +0900

    Wheelをクラスに切り出す

--- bicycle/gear.rb
+++ bicycle/gear.rb
@@ -1,10 +1,12 @@
+require './bicycle/wheel'
+
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
@@ -14,13 +16,9 @@ class Gear
   def gear_inches
     ratio * wheel.diameter
   end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + tire * 2
-    end
-  end
 end
 
-puts Gear.new(52, 11, 0, 0).ratio
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+wheel = Wheel.new(26, 1.5)
+
+puts Gear.new(52, 11).ratio
+puts Gear.new(52, 11, wheel).gear_inches
