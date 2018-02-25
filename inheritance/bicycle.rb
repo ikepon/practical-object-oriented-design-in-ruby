2f6e4766acf67d115d4072ac8f39f253675b6c7c
ikepon <tatsuyanoheya@gmail.com>
2018-02-25 16:57:31 +0900

    共通パーツをBicycleに移動

--- inheritance/bicycle.rb
+++ inheritance/bicycle.rb
@@ -1,4 +1,11 @@
 class Bicycle
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(args)
+    @size = args[:size]
+    @chain = args[:chain]
+    @tire_size = args[:tire_size]
+  end
 end
 
 class MountainBike < Bicycle
