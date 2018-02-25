40432ae47aaa547e0e1e0278d59dd1a77f9e4df7
ikepon <tatsuyanoheya@gmail.com>
2018-02-25 17:21:44 +0900

    superをフックメソッドで削除

--- inheritance/bicycle.rb
+++ inheritance/bicycle.rb
@@ -5,13 +5,23 @@ class Bicycle
     @size = args[:size]
     @chain = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
+
+    post_initialize(args)
+  end
+
+  def post_initialize(args)
+    nil
   end
 
   def spares
     {
       tire_size: tire_size,
       chain: chain
-    }
+    }.merge(local_spares)
+  end
+
+  def local_spares
+    {}
   end
 
   def default_chain
@@ -26,14 +36,13 @@ end
 class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shock
 
-  def initialize(args)
+  def post_initialize(args)
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
-    super(args)
   end
 
-  def spares
-    super.merge(rear_shock: rear_shock)
+  def local_spares
+    { rear_shock: rear_shock }
   end
 
   def default_tire_size
@@ -44,13 +53,12 @@ end
 class RoadBike < Bicycle
   attr_reader :size, :tape_color
 
-  def initialize(args)
+  def post_initialize(args)
     @tape_color = args[:tape_color]
-    super(args)
   end
 
-  def spares
-    super.merge(tape_color: tape_color)
+  def local_spares
+    { tape_color: tape_color }
   end
 
   def default_tire_size
@@ -61,13 +69,12 @@ end
 class RecumbentBike < Bicycle
   attr_reader :flag
 
-  def initialize(args)
-    @flag = flag
-    super(args)
+  def post_initialize(args)
+    @flag = args[:flag]
   end
 
-  def spares
-    super.merge(flag: flag)
+  def local_spares
+    { flag: flag }
   end
 
   def default_chain
@@ -84,4 +91,5 @@ puts mountain_bike.tire_size
 puts mountain_bike.chain
 puts mountain_bike.spares
 
-bent = RecumbentBike.new(size: 'S')
+bent = RecumbentBike.new(flag: 'tall and orange')
+puts bent.spares
